c45b584c42f60e83c4c62dbd39ee8941b0306ef8
demirbileku <udemirbilek2@gmail.com>
2021-07-08 12:44:30 +0300

    commit tree 0fb2da2e930e0df4da2eaf30d9dd3719bdac0aee

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,23 @@
 import React from "react";
 
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">Home</a>
-      <a href="#about">About</a>
-    </nav>
-  );
+class NavBar extends React.Component{
+  render(){
+    return (
+      <nav>
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+      </nav>
+    )
+  }
 }
+
+// function NavBar() {
+//   return (
+//     <nav>
+//       <a href="#home">Home</a>
+//       <a href="#about">About</a>
+//     </nav>
+//   );
+// }
 
 export default NavBar;
